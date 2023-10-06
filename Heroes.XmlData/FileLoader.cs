deb9415f314146d02d20f419291df2cfbb7d0916
Kevin Oliva <kevin.k.oliva@gmail.com>
2023-10-06 14:22:04 -0500

    Add project files.

---
+++ Heroes.XmlData/FileLoader.cs
@@ -0,0 +1,13 @@
+﻿namespace Heroes.XmlData;
+
+public class FileLoader
+{
+    private FileLoader()
+    {
+    }
+
+    public static FileLoader GetInstance()
+    {
+        return new FileLoader();
+    }
+}
---
+++ Heroes.XmlData/GameData/FileGameData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Heroes.XmlData.GameData
+{
+    internal class FileGameData : AbstractGameData
+    {
+        public FileGameData(string modsDirectoryPath)
+            : base(modsDirectoryPath)
+        {
+        }
+
+        public FileGameData(string modsDirectoryPath, int hotsBuild)
+            : base(modsDirectoryPath, hotsBuild)
+        {
+        }
+    }
+}
