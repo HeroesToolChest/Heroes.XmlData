296c0e5a93b864b8700dadf0a04418a16d2c0b9b
Kevin Oliva <kevin.k.oliva@gmail.com>
2024-03-17 17:50:51 -0500

    updated parameter to Path

--- Heroes.XmlData/StormData/GameStringText.cs
+++ Heroes.XmlData/StormData/GameStringText.cs
@@ -4,5 +4,5 @@
 /// Contains the properties for a gamestring.
 /// </summary>
 /// <param name="Value">The value of the gamestring.</param>
-/// <param name="FilePath">The file path where the gamestring resides from.</param>
-public record GameStringText(string Value, string FilePath) : StormStringValue(Value, FilePath);
+/// <param name="Path">The file path where the gamestring resides from.</param>
+public record GameStringText(string Value, string Path) : StormStringValue(Value, Path);
