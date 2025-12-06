c84145a3459b175b507adf18c35cd3e68945b169
Kevin Oliva <kevin.k.oliva@gmail.com>
2025-12-05 23:02:13 -0600

    update

--- Heroes.XmlData.Benchmarks/XmlDataBenchmarks.cs
+++ Heroes.XmlData.Benchmarks/XmlDataBenchmarks.cs
@@ -1,10 +1,13 @@
 ﻿using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Jobs;
 
 namespace Heroes.XmlData.Benchmarks;
 
 [MemoryDiagnoser]
-[SimpleJob(RuntimeMoniker.Net90)]
 public class XmlDataBenchmarks
 {
+    [Benchmark]
+    public void TestMethod()
+    {
+        // Your benchmarking code here
+    }
 }
